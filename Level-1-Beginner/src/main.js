0b05a965854fc7a56ea2d93d96f15b039ace94a6
YooJin Chang <yoojinchang@YooJins-MBP.home>
2022-04-18 15:12:50 -0400

    level 1 beginner done

--- Level-1-Beginner/src/main.js
+++ Level-1-Beginner/src/main.js
@@ -2,15 +2,15 @@
 // ex: addTwo(5); -> 7
 // ex: addTwo(6); -> 8
 function addTwo(number) {
-  // CODE HERE
-
+  return number + 2;
+  
 }
 
 
 // adds the two inputted numbers together
 // ex: add(5, 10); -> 15
 function add(num1, num2) {
-  // CODE HERE
+  return num1 + num2
 
 }
 
@@ -19,7 +19,7 @@ function add(num1, num2) {
 // ex: addd(1,2,3); -> 6
 // ex: addd(2,4,2); -> 8
 function addd(value1, value2, value3) {
-  // CODE HERE
+  return value1 + value2 + value3
 
 }
 
@@ -27,7 +27,7 @@ function addd(value1, value2, value3) {
 // subtracts the second inputted number from the first inputted number
 // ex: subtract(5, 10); -> -5
 function subtract(n1, n2) {
-  // CODE HERE
+  return n1 - n2
 
 }
 
@@ -36,7 +36,7 @@ function subtract(n1, n2) {
 // ex: multiplyByTwo(5); -> 10
 // ex: multiplyByTwo(6); -> 12
 function multiplyByTwo(numero) {
-  // CODE HERE
+ return numero * 2
 
 }
 
@@ -45,8 +45,7 @@ function multiplyByTwo(numero) {
 // ex: multiply(1,2); -> 2
 // ex: multiply(2,2); -> 4
 function multiply(val1, val2) {
-  // CODE HERE
-
+  return val1 * val2
 }
 
 
@@ -54,7 +53,7 @@ function multiply(val1, val2) {
 // ex: multiplyy(1,2,3); -> 6
 // ex: multiplyy(2,2,3); -> 12
 function multiplyy(a, b, c) {
-  // CODE HERE
+  return a * b * c;
 
 }
 
@@ -62,7 +61,7 @@ function multiplyy(a, b, c) {
 // returns the square of the inputted number
 // ex: square(5); -> 25
 function square(value) {
-  // CODE HERE
+  return value * value;
 
 }
 
@@ -74,7 +73,8 @@ function square(value) {
 // ex: isOdd(5); -> true
 function isOdd(num) {
   // CODE HERE
-
+if (num % 2 === 0) return false;
+  return true;
 }
 
 
@@ -85,7 +85,8 @@ function isOdd(num) {
 // ex: isNegative(1); -> false
 // ex: isNegative(0); -> false
 function isNegative(num) {
-  // CODE HERE
+  if (num >= 0) return false;
+  return true;
 
 }
 
@@ -94,15 +95,15 @@ function isNegative(num) {
 // ex: positive(-1); -> 1
 // ex: positive(1); -> 1
 function positive(num) {
-  // CODE HERE
-
+  return Math.abs(num)
 }
 
 // Returns boolean of whether argument is classified as a Number object
 // isNumber(5); → true
 // isNumber('hi'); → false
 function isNumber(value) {
-  // CODE HERE
+  if (typeof value === "number") return true;
+    return false;
 
 }
 
@@ -110,15 +111,16 @@ function isNumber(value) {
 // isArray(5); → false
 // isArray([1,2,3]); → true
 function isArray(value) {
-  // CODE HERE
-
+  if (Array.isArray(value)) return true;
+    return false;
 }
 
 // Returns boolean of whether argument is classified as an Object
 // isObject(5); → false
 // isObject([1,2,3]); → true
 function isObject(value) {
-  // CODE HERE
+  if (typeof value === "object") return true;
+    return false;
 
 }
 
@@ -126,7 +128,8 @@ function isObject(value) {
 // isNull(null); -> true
 // isNull(5); -> false
 function isNull(value) {
-  // CODE HERE
+  if (value === null) return true;
+    return false;
 
 }
 
